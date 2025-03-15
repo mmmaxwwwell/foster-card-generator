43a358c82e746430742331f2346afcdec926c0c4
mmmaxwwwell <mmmaxwwwell@protonmail.com>
2025-03-15 16:25:29 -0400

    template works

--- api/src/generate-cards.js
+++ api/src/generate-cards.js
@@ -2,16 +2,18 @@ const puppeteer = require('puppeteer');
 const path = require('path');
 
 async function capture(page, divName) {
-    const div = await page.$("#" + divName);
+    const div = await page.$("#page");
     const bounding_box = await div.boundingBox();
     await page.screenshot({
-        path: "output/" +divName + ".png",
+        path: "output/" + divName + ".jpg",
         clip: {
             x: bounding_box.x,
             y: bounding_box.y,
             width: bounding_box.width,
             height: bounding_box.height
-        }
+        },
+        type: 'jpeg',
+        quality: 80 // You can adjust quality from 0-100
     });
 }
 
@@ -24,9 +26,10 @@ async function capture(page, divName) {
         deviceScaleFactor: 2
     })
 
-    await page.goto(`file://${path.join(process.cwd(), 'src/card.html')}`, { waitUntil: 'networkidle0' });
-
+    await page.goto(`file://${path.join(process.cwd(), 'src/card-front.html')}`, { waitUntil: 'networkidle0' });
     await capture(page, "card-front");
+
+    await page.goto(`file://${path.join(process.cwd(), 'src/card-back.html')}`, { waitUntil: 'networkidle0' });
     await capture(page, "card-back");
 
     await browser.close();
